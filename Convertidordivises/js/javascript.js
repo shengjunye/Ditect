e1de64ea529b0cf7b045f4fc8c0dc8619c203b05
ShengJun Ye <shengjun.ye@mataro.epiaedu.cat>
2019-03-09 13:35:06 +0100

    Projectefinal

--- Convertidordivises/js/javascript.js
+++ Convertidordivises/js/javascript.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $.getJSON("js/divises.json", function(data){
-        console.log(data);
+//Funció per a convertir les monedes
+function conversor(){
+    $.getJSON("http://data.fixer.io/api/latest?access_key=e811890ea697f98d53810327219e42d9", function(data){
+        var euros = document.getElementById("import").value;
+        console.log(euros);
+        var moneda = document.getElementById("divises").value;
+        console.log(moneda);
+        var resultat = euros * data["rates"][moneda];
+        console.log(resultat);
+        resultat = resultat.toFixed(2);
+        console.log(resultat);
+        document.getElementById("total").value = resultat;
     });
-});
+}
